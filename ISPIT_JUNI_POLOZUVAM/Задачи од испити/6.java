43b8f4598b77407f3a43472a1d8073c15fb1e21e
Filip Iliev <151552696+grga1@users.noreply.github.com>
2025-06-08 02:43:35 +0200

    Update 6.java

--- ISPIT_JUNI_POLOZUVAM/Задачи од испити/6.java
+++ ISPIT_JUNI_POLOZUVAM/Задачи од испити/6.java
@@ -1,3 +1,7 @@
 // vleznata beshe so drvo ista ko 
 // laboratoriskite so root add i ask i 
 // trebashe da najdesh prosek od stepen na jazol, sho e ustvari (zbir od jazlite)/(zbir od jazlite + 1)
+
+
+
+// ednata grupa prosecna dlabocina na poddrvoto na toj jazol, drugata grupa prosecen stepen
