8a7efb4a003d99a9d4351b0530e9da4d1d6cf1a6
Filip Iliev <151552696+grga1@users.noreply.github.com>
2025-06-12 04:45:02 +0200

    Update 1.java

--- ISPIT_JUNI_POLOZUVAM/5.1 Хеш табела/1.java
+++ ISPIT_JUNI_POLOZUVAM/5.1 Хеш табела/1.java
@@ -1,1 +1,21 @@
+// Задача 1. Родендени
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
+   HashMap<String,Integer> mapa = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String[] datum = scanner.next().split("\\.");
+         mapa.put(datum[1], mapa.getOrDefault(datum[1],0)+1);
+        }
+        String datum = scanner.next();
+        if (mapa.containsKey(datum)){
+            System.out.println(mapa.get(datum));
+        }else{
+            System.out.println(0);
+        }
+    }
+}
